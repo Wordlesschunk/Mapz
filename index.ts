b359611fcfaac42d7e321badde5ab1ea714ad9b6
dylan <dylan.jennings@wrenkitchens.com>
2022-05-02 00:20:44 +0100

    added pins

--- index.ts
+++ index.ts
@@ -12,8 +12,8 @@ function initMap(): void {
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
-      zoom: 12,
-      center: { lat: 34.84555, lng: -111.8035 },
+      zoom: 6,
+      center: { lat: 53.566444, lng: -1.874753 },
     }
   );
 
@@ -21,12 +21,47 @@ function initMap(): void {
   // receives the initial focus when tab is pressed. Use arrow keys to
   // move between markers; press tab again to cycle through the map controls.
   const tourStops: [google.maps.LatLngLiteral, string][] = [
-    [{ lat: 34.8791806, lng: -111.8265049 }, "Boynton Pass"],
-    [{ lat: 34.8559195, lng: -111.7988186 }, "Airport Mesa"],
-    [{ lat: 34.832149, lng: -111.7695277 }, "Chapel of the Holy Cross"],
-    [{ lat: 34.823736, lng: -111.8001857 }, "Red Rock Crossing"],
-    [{ lat: 34.800326, lng: -111.7665047 }, "Bell Rock"],
+    //  UK
+    [{ lat: 53.566630, lng: -0.079821 }, "Grimsby"],
+    [{ lat: 53.229010, lng: -0.540006 }, "Lincoln"],
+    [{ lat: 53.767180, lng: -0.327435 }, "Hull"],
+    [{ lat: 53.343777, lng: 0.019004 }, "Kenwick Park"],
+    [{ lat: 53.798561, lng: -1.549767 }, "Leeds"],
+    [{ lat: 53.960800, lng: -1.073879 }, "York"],
+    [{ lat: 53.479575, lng: -2.243575 }, "Manchester"],
+    [{ lat: 53.353428, lng: -2.275718 }, "Manchester Airport"],
+    [{ lat: 53.412937, lng: -1.409038 }, "Meadowhall"],
+    [{ lat: 53.1922937, lng: 0.347634 }, "Fantasy Island"],
+    [{ lat: 53.147219, lng: 0.338120 }, "Skegness"],
+    [{ lat: 53.638056, lng: -0.659328 }, "Go Ape Normanby Hall"],
+    [{ lat: 53.586574, lng: -0.654850 }, "Scunthorpe"],
+    [{ lat: 53.378611, lng: -1.471450 }, "Sheffield"],
+    [{ lat: 51.505103, lng: -0.1263990 }, "London"],
+
+    // NL
+    [{ lat: 51.948761, lng: 4.137080 }, "Europoort Rotterdam "],
+    [{ lat: 52.368199, lng: 4.900762 }, "Amsterdam - DATE "],
+
+    // PAR
+    [{ lat: 49.008708, lng: 2.550542 }, "Paris Charles de Gaulle Airport "],
+    [{ lat: 48.855649, lng: 2.351819 }, "Paris - DATE "],
+    [{ lat: 48.867209, lng: 2.783808 }, "Disneyland Paris - DATE"],
+
+    // FUE
+    [{ lat: 28.448104, lng: -13.866353 }, "Fuerteventura Airport"],
+    [{ lat: 28.072954, lng: -14.306402 }, "Esquinzo / Club Jandía Princess "],
+    [{ lat: 28.060693, lng: -14.319502 }, "Las Gaviotas - sep 5 21"],
+    [{ lat: 28.187251, lng: -14.157065 }, "Oasis Wildlife Fuerteventura - sep 7 21"],
   ];
+
+
+
+
+
+
+
+
+
 
   // Create an info window to share between markers.
   const infoWindow = new google.maps.InfoWindow();
